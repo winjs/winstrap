ceb96266c4cf58a7da3afc0b2872dc2d6a77040f
David Evans <david0evans@gmail.com>
2015-03-30 11:35:12 -0700

    Fix grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    'dist/css/app.css': 'src/scss/app.scss'
+                    'dist/css/bootstrap-winjs.css': 'src/scss/bootstrap-winjs.scss'
                 }
             }
         },
